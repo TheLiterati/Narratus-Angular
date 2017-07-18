b2fafd0a19c583da5de46170347b139c39ad5ca3
Caleb <caleb.wells1@gmail.com>
2017-07-18 12:37:00 -0700

    testing travis

--- karma.conf.js
+++ karma.conf.js
@@ -1,5 +1,3 @@
-// Karma configuration
-// Generated on Mon Jul 17 2017 16:37:34 GMT-0700 (PDT)
 'use strict';
 
 const webpack = require('./webpack.config.js');
@@ -8,12 +6,8 @@ delete webpack.entry;
 module.exports = function(config) {
   config.set({
     webpack,
-    // base path that will be used to resolve all patterns (eg. files, exclude)
     basePath: '',
-    // frameworks to use
-    // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
     frameworks: ['mocha'],
-    // list of files / patterns to load in the browser
     files: [
       'node_modulus/babel-polyfill/dist/polyfill.js',
       'node_modules/angular/angular.js',
@@ -21,11 +15,8 @@ module.exports = function(config) {
       'app/entry.js',
       'test/**/*-test.js',
     ],
-    // list of files to exclude
     exclude: [
     ],
-    // preprocess matching files before serving them to the browser
-    // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {
       'app/entry.js': ['webpack'],
       'test/**/*-test.js': ['webpack'],
@@ -36,27 +27,13 @@ module.exports = function(config) {
         flags: ['--no-sandbox'],
       },
     },
-    // test results reporter to use
-    // possible values: 'dots', 'progress'
-    // available reporters: https://npmjs.org/browse/keyword/karma-reporter
     reporters: ['progress'],
-    // web server port
     port: 9876,
-    // enable / disable colors in the output (reporters and logs)
     colors: true,
-    // level of logging
-    // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
     logLevel: config.LOG_INFO,
-    // enable / disable watching file and executing tests whenever any file changes
     autoWatch: true,
-    // start these browsers
-    // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
     browsers: ['Chrome'],
-    // Continuous Integration mode
-    // if true, Karma captures browsers, runs the tests and exits
     singleRun: false,
-    // Concurrency level
-    // how many browser should be started simultaneous
     concurrency: Infinity,
   });
 };
