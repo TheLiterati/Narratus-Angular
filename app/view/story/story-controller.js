a549b370b3d62b6a336b69d565320d9caf78b671
Allie Grampa <agrampa@yahoo.com>
2017-07-27 11:54:16 -0700

    working on toggling views with ng-if and ng-click in story.html

--- app/view/story/story-controller.js
+++ app/view/story/story-controller.js
@@ -9,7 +9,29 @@ module.exports = [
 
       this.title = 'Welcome to Narratus';
 
-      // let url = $location.url();
+      let url = $location.url();
+      this.read = url === '/story#view';
+      this.edit = url === '/story#edit';
+      this.create = url === '/story#create';
+
+      this.readStory = () => {
+        this.read = true;
+        this.edit = false;
+        this.create = false;
+        console.log('in read story');
+      };
+
+      this.createStory = () => {
+        this.read = false;
+        this.edit = false;
+        this.create = true;
+      };
+
+      this.editStory = () => {
+        this.read = false;
+        this.edit = true;
+        this.create = false;
+      };
     };
   },
 ];
